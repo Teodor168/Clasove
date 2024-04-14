376083e3c9d41bba45e32e25d988086012d6dd1c
Teodor168 <teodor16iliev@gmail.com>
2024-04-14 22:34:48 +0300

    neshto

--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -5,18 +5,22 @@
         static void Main(string[] args)
         {
             byte n = byte.Parse(Console.ReadLine());
-            List<object> families = new List<object>();
+            List<Person> families = new List<Person>(); 
+           
             for (int i = 0; i < n; i++)
             {
-                Person person = new Person();              
-                for (int j = 0; j < n; j++)
-                {
+                             Person person = new Person(); 
+                
                     person.name = Console.ReadLine();
                     person.age =int.Parse (Console.ReadLine());
-                }
+                
                 families.Add(person);
             }
-            Console.WriteLine(string.Join("\n", families));
+
+            for (int i = 0;i < families.Count;i++)
+            {
+                Console.WriteLine(string.Join(" ", families[i].name, families[i].age));
+            }
         }
-    }//ne sme cigani da pravim novi objecti (dqca) sus sushtite imena
+    }
 }
--- Zad3/obj/Debug/net8.0/Zad3.AssemblyInfo.cs
+++ Zad3/obj/Debug/net8.0/Zad3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zad3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395328ee47a99f7b926a615e152fabbbc29ef395")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zad3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zad3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
